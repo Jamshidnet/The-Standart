fa16f589f059f854fb56bcc2069e09a0c2eec8f2
Jamshidnet <122851582+Jamshidnet@users.noreply.github.com>
2023-05-26 00:07:39 +0500

    Add files via upload

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -0,0 +1,122 @@
+﻿//===================================================
+// Copyright (c)  coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Pease
+//===================================================
+
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Brokers.DateTimes;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Services.Foundations.Guests;
+using Tynamix.ObjectFiller;
+using Xeptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IGuestService guestService;
+
+        public GuestServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.guestService = new GuestService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        public static TheoryData<int> InvalidSeconds()
+        {
+            int secondsInPast = -1 * new IntRange(
+                min: 60,
+                max: short.MaxValue).GetValue();
+
+            int secondsInFuture = new IntRange(
+                min: 0,
+                max: short.MaxValue).GetValue();
+
+            return new TheoryData<int>
+            {
+                secondsInPast,
+                secondsInFuture
+            };
+        }
+
+        private static Guest CreateRandomGuest(DateTimeOffset dates) =>
+            CreateGuestFiller(dates).Create();
+
+        private static Guest CreateRandomGuest() =>
+            CreateGuestFiller(GetRandomDateTimeOffset()).Create();
+
+        private static Guest CreateRandomModifyGuest(DateTimeOffset dates)
+        {
+            int randomDaysInPast = GetRandomNegativeNumber();
+            Guest randomGuest = CreateRandomGuest(dates);
+
+            randomGuest.CreatedDate = randomGuest.CreatedDate.AddDays(randomDaysInPast);
+
+            return randomGuest;
+        }
+
+        private static IQueryable<Guest> CreateRandomGuests()
+        {
+            return CreateGuestFiller(dates: GetRandomDateTimeOffset())
+                .Create(count: GetRandomNumber()).AsQueryable();
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 99).GetValue();
+
+        private static int GetRandomNegativeNumber() =>
+            -1 * new IntRange(min: 2, max: 10).GetValue();
+
+        private static string GetRandomString() =>
+            new MnemonicString().GetValue();
+
+        private static SqlException CreateSqlException() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
+
+        private static SqlException GetSqlError() =>
+            (SqlException)FormatterServices.GetUninitializedObject(typeof(SqlException));
+
+        private static T GetInvalidEnum<T>()
+        {
+            int randomNumber = GetRandomNumber();
+            while (Enum.IsDefined(typeof(T), randomNumber) is true)
+            {
+                randomNumber = GetRandomNumber();
+            }
+            return (T)(object)randomNumber;
+        }
+
+        private static string GetRandomMessage() =>
+            new MnemonicString(wordCount: GetRandomNumber()).GetValue();
+
+        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+
+        private static Filler<Guest> CreateGuestFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Guest>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
